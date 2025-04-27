de258f6857066c6cecdc4a8bb650b322ca23c197
Pedro Amorim <amorimp@proton.me>
2025-04-26 23:22:26 -0300

    refactor: extract constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+import os
+
+BUILD_INFO_FILE = "build_info.json"
+BUILD_INFO_KEY = "version"
+NOTIFICATION_TIMEOUT = 5000
+DISCORD_DEB_URL = "https://discord.com/api/download?platform=linux&format=deb"
+HEADERS_PROPERTY = "location"
+DISCORD_DEFAULT_PATHS = [
+    "/usr/share/discord/",
+    "/opt/discord/",
+    os.path.expanduser("~/.local/share/discord/")
+]
